502fb089c0230edc0e210ff89ed82c2c2d8b13a5
Duc Tran <minhductran.ntt@gmail.com>
2019-06-01 02:59:42 -0500

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+    def sign_in
+        redirect_to root_path
+    end
+end
---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,10 @@
+class WelcomeController < ApplicationController
+    def index
+        @name = :dan
+    end
+
+    def about
+        @name = :duc
+    end
+    
+end
