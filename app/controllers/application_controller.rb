6e7137fd226e76a16333a04802572b67f7aee028
Duc Tran <ducmtran@Mac.local>
2021-12-03 21:35:33 -0800

    change something

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::Base
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :username, :password])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:login, :username, :remember_me])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:login])
   end
 end
